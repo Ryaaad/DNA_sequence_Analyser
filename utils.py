0e005151cc18de8b32480e780f132c74f41fe4ab
Ryaaad <devil.fine116@gmail.com>
2024-01-28 19:04:32 +0100

    finishing all the utils functions

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from random import randint
+
 def randomADN(leng):
     Seq=''
     for i in range(leng):
@@ -14,13 +15,20 @@ def randomADN(leng):
     return Seq          
 
 def VerifySeq(seq) : 
-   verified=True
    for i in seq : 
-     if(i!='T' or i!='A' or i!='G' or i!='C') :
-        verified=False
-   return verified     
+     if(i!='T' and i!='A' and i!='G' and i!='C') :
+       return False
+   return True     
    
-def ADNseq_FromFile(path):
+def taux_GC(seq):
+ GC=0
+ for i in seq :
+   if i=="C" or i=="G":
+    GC+=1
+ GC=GC/len(seq) * 100    
+ return GC
+
+def ADNseq_From_File(path):
  f=open(path,'r')
  seq=f.read()
  return seq
@@ -39,7 +47,7 @@ def Nucleotide_Occurrences(seq):
         Cocc+=1 
      if i=="G":
         Gocc+=1   
-   print(f" T : {Tocc}  \n A : {Aocc} \n C : {Cocc} \n G : {Gocc} ")
+   return {"Adénine" : {Aocc},"Thymine" : {Tocc},"Cytosine" : {Cocc},"Guanine" : {Gocc}}
 
 def Adn_complementary(seq):
    compSeq=''
@@ -54,6 +62,41 @@ def Adn_complementary(seq):
        compSeq+="C" 
    return compSeq    
 
+def Adn_Mutation(seq,number):
+    T=[-1]
+    NewSeq=seq
+    if number>=len(seq):
+       Newseq=randomADN(seq)
+    for i in range(number):
+     x=randint(0,len(seq))
+     print(x)
+     j=0 
+     Same=True
+     while j<i and Same==True:
+       print("boucle while here")
+       if x==T[j] :
+         print("t")
+         x=randint(0,len(seq))
+         j=+1
+       else : 
+        print("true")
+        Same=False
+    for i in T : 
+     Newseq[i]='R' #to fix
+    return NewSeq  
+
+def Chercher_motif(seq,sousSeq):
+  Tindex=[]
+  for i in range(len(seq)) : 
+     if seq[i]==sousSeq[0]:
+       if len(seq)>=len(sousSeq)+i :
+        j=0
+        while j<len(sousSeq) and seq[i+j]==sousSeq[j]:
+         j=j+1
+        if j>=len(sousSeq):
+         Tindex.append(i)
+  return Tindex     
+
 def Adn_to_Arn(seq): # maybe no need for Adn_complementary
    compSeq=Adn_complementary(seq)
    ARNseq=''
@@ -64,60 +107,75 @@ def Adn_to_Arn(seq): # maybe no need for Adn_complementary
          ARNseq+=i  
    return ARNseq     
 
-# def Arn_to_Protein(seq) :
-#    Protein=''
-#    for i in range(0, len(seq),3) :
-#       souseq=seq[i]
-#       if i+2<len(seq) :
-#         souseq=souseq+seq[i+1]+seq[i+2]
-#       elif i+1<len(seq) :
-#         souseq=souseq+seq[i+1]
-#       if souseq=="AAA" :
-#         Protein=Protein+'-'+''
-#       print(f"{souseq} ")   
-      
+def fréquences_codons(seq):
+  T=[]
+  fréqDict={}
+  cmp=0
+  for i in range(0,len(seq),3) :
+    if i+2<len(seq) :
+     T.append(seq[i]+seq[i+1]+seq[i+2]) 
+     cmp+=1
+  Temp=T[:]
+  for i in range(cmp):
+    for j in range(i+1,cmp) :
+      if T[i]==T[j] and T[i]!=0 :
+         T[j]=0
+  
+  for i in range(cmp):
+    if T[i]!=0 : 
+      fréqDict[T[i]]=0
+      for j in range(i,cmp):
+        if(T[i]==Temp[j]) :
+          fréqDict[T[i]]=fréqDict[T[i]]+1
+  return fréqDict
+
+def Arn_to_Protein(seq) :
+   Protein=''
+   for i in range(0, len(seq),3) :
+      souseq=seq[i]
+      if i+2<len(seq) :
+        souseq=souseq+seq[i+1]+seq[i+2]
+      elif i+1<len(seq) :
+        souseq=souseq+seq[i+1]
+      if souseq=="UUU" or  souseq=="UUC" :
+        Protein=Protein+'-'+'Phe'
+      if souseq=="UCU" or  souseq=="UCC" or  souseq== "UCA" or  souseq=="UCG" :
+        Protein=Protein+'-'+'Ser'        
+      if souseq=="UAU" or  souseq=="UAC":
+       Protein=Protein+'-'+'Tyr'        
+      if souseq=="UGG" : 
+       Protein=Protein+'-'+'Trp'        
+      if souseq=="UAA" or  souseq=="UAG" or  souseq=="UGA" : 
+       Protein=Protein+'-'+'Trp'        
+
+   Protein=Protein[1:len(Protein)] 
+   return Protein 
+
+# s='TAATAGATTTTA'
+# arn=Adn_to_Arn(s)
+# pro=Arn_to_Protein(arn)
+# print(arn)      
+# print(pro)
 # UUU or UUC: Phenylalanine (Phe)
-
 # UCU, UCC, UCA, UCG: Serine (Ser)
-
 # UAU or UAC: Tyrosine (Tyr)
-
 # UGG: Tryptophan (Trp)
-
+       
 # UAA, UAG, UGA: Stop codons (end translation)
-
 # CUU, CUC, CUA, CUG: Leucine (Leu)
-
 # CCU, CCC, CCA, CCG: Proline (Pro)
-
 # CAU or CAC: Histidine (His)
-
 # CAA or CAG: Glutamine (Gln)
-
 # CGU, CGC, CGA, CGG: Arginine (Arg)
-
 # AUU, AUC, AUA: Isoleucine (Ile)
-
 # ACU, ACC, ACA, ACG: Threonine (Thr)
-
 # AAU or AAC: Asparagine (Asn)
-
 # AAA or AAG: Lysine (Lys)
-
 # AGU or AGC: Serine (Ser)
-
 # AGA or AGG: Arginine (Arg)
-
 # GUU, GUC, GUA, GUG: Valine (Val)
-
 # GCU, GCC, GCA, GCG: Alanine (Ala)
-
 # GAU or GAC: Aspartic Acid (Asp)
-
 # GAA or GAG: Glutamic Acid (Glu)
-
 # GGU, GGC, GGA, GGG: Glycine (Gly)
-
 # AUG: Methionine (Met) - Start codon
-
-# UAA, UAG, UGA: Stop codons - End translation
